9084fbb8b3bbedabea8eedcfe1b973ab7d0a7f7a
juanjo giraldez <juanjose.giraldez@kairosds.com>
2020-08-24 13:29:28 +0200

    First folder (components) and added files

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            <h1>Esto es la vista de Home</h1>
+            <hr/>
+            
+        </div>
+    )
+}
